485d894dfdc28b91eab5dd4e46840f0a65f57348
rup-ff <86917835+rup-ff@users.noreply.github.com>
2022-02-17 15:36:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 song="";
-
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload()
 {
 song=loadSound("music.mp3");
+
 }
 
 function setup()
@@ -11,8 +15,28 @@ function setup()
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on("pose" , gotPoses);
+    
 }
+function modelLoaded()
+{
+    console.log("Posenet is initialized");
+}
+function gotPoses(results)
+{
+    if(results.length > 0 )
+    {
+    console.log(results);
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX=" + leftWristX + "leftWristY" + leftWristY);
 
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX=" + rightWristX + "rightWristY" + rightWristY);
+    }
+}
 
 function draw()
 {
@@ -21,4 +45,6 @@ function draw()
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
