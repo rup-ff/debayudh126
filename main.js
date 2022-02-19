84a5e369a3a3dcae4ca50c231ffa57d7fca0d7d7
rup-ff <86917835+rup-ff@users.noreply.github.com>
2022-02-19 10:47:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -28,9 +28,9 @@ function gotPoses(results)
     if(results.length > 0 )
     {
     console.log(results);
-    scoreLeftWrist = result[0].pose.keypoints[9].score;
+    scoreLeftWrist = results[0].pose.keypoints[9].score;
     console.log("scoreLeftWrist = " + scoreLeftWrist);
-    
+
     leftWristX = results[0].pose.leftWrist.x;
     leftWristY = results[0].pose.leftWrist.y;
     console.log("leftWristX=" + leftWristX + "leftWristY" + leftWristY);
